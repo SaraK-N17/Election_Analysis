f57d83ac9f35c232bef6d92347e5101505d10c25
Kloft-Nelson <Kloft-Nelson.Sara@mayo.edu>
2022-01-12 18:16:37 -0600

    Adding the election audit code.

--- PyPoll.py
+++ PyPoll.py
@@ -22,25 +22,87 @@ import csv
 import os
 # Assign a variable for the file to load and the path.
 file_to_load = os.path.join("Resources", "election_results.csv")
-# Open the election results and read the file.
-with open(file_to_load) as election_data:
-# To do: read and analyze the data here.
+# Create a filename variable to a direct or indirect path to the file.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
 
-    # Read the file object with the reader function.
+# Initialize a total vote counter
+total_votes = 0
+
+# Candidate Options
+candidate_options = []
+# 1. Declare the empty dictionary.
+candidate_votes = {}
+# Winning Candidate and Winning Count Tracker
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
+# Open and read the election results and read the file.
+with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
+    # Read and print the header row.
+    headers = next(file_reader)
+    # print(headers)
 
     # Print each row in the CSV file.
     for row in file_reader:
-        print(row)
+    #print(row)
+    #for row in file_reader:
+        # Add to the total vote count
+        total_votes += 1
+    # Print the candidate name from each row
+        candidate_name = row[2]
+    # If the candidate does not match any existing candidate...
+        if candidate_name not in candidate_options:
 
-   # Read and print the header row.
-    headers = next(file_reader)
-    print(headers)
-      
+            # Add it to the list of candidates.
+            candidate_options.append(candidate_name)
+
+            # Begin tracking that candidate's vote count.
+            candidate_votes[candidate_name] = 0
+
+        # Add a vote to that candidate's count
+        candidate_votes[candidate_name] += 1
+
+    # Determine the percentage of votes for each candidate by looping through the counts.
+    # Iterate through the candidate list.
+    for candidate_name in candidate_votes:
+
+        # Retrieve vote count of a candidate.
+        votes = candidate_votes[candidate_name]
+
+        # Calculate the percentage of votes.
+        vote_percentage = float(votes) / float(total_votes) * 100
+
+        # Determine winning vote count and candidate
+        # Determine if the votes is greater than the winning count.
+        if (votes > winning_count) and (vote_percentage > winning_percentage):
+            # If true then set winning_count = votes and winning_percent =
+            # vote_percentage.
+            winning_count = votes
+            winning_percentage = vote_percentage
+            # And, set the winning_candidate equal to the candidate's name.
+            winning_candidate = candidate_name
+
+        # To do: print out each candidate's name, vote count, and percentage of
+        # votes to the terminal.
+        print(f"{candidate_name}: received {vote_percentage:.1f}% ({votes:,})\n")
+
+    winning_candidate_summary = (
+    f"-------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"-------------------------\n")
+    print(winning_candidate_summary)
 
 
-# Create a filename variable to a direct or indirect path to the file.
-file_to_save = os.path.join("analysis", "election_analysis.txt")
+#Print candidate options
+#print(candidate_options)
+
+# Print the candidate vote dictionary.
+#print(candidate_votes)
+
 # Using the open() function with the "w" mode we will write data to the file.
 open(file_to_save, "w")
 
@@ -52,6 +114,5 @@ with open(file_to_save, "w") as txt_file:
 
     # Write three counties to the file
     txt_file.write("Counties in the Election\n__________________________\nArapahoe\nDenver\nJefferson")
-
 
 # Close the file
--- Python_practice2.py
+++ Python_practice2.py
@@ -25,6 +25,8 @@ counties_dict["Arapahoe"] = 422829
 counties_dict["Denver"] = 463353
 counties_dict["Jefferson"] = 432438
 
+
+
 voting_data = []
 voting_data.append({"county":"Arapahoe", "registered_voters": 422829})
 voting_data.append({"county":"Denver", "registered_voters": 463353})
